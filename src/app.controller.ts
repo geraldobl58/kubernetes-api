87ef8354f9db2dd08c729013cd6ba449916ab64b
Geraldo Luiz <geraldobl58@gmail.com>
2025-02-18 11:34:12 -0300

    add Kubernetes endpoint to AppController and corresponding service method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/k8s')
+  getKubernetes(): string {
+    return this.appService.getKubernetes();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getKubernetes(): string {
+    return 'Hello Kubernetes!';
+  }
 }
